ab05e4da5210c46c36c146c650a1a4f2a19bf7d4
Quân <92642706+QunNguyen@users.noreply.github.com>
2022-08-10 10:45:16 +0700

    up

--- App.js
+++ App.js
@@ -7,7 +7,6 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -31,18 +30,20 @@ import Login from './Screen/Login';
 import Tabs from './Screen/Tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
 const tab=createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    // <NavigationContainer >
-    //   <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown :false}}>
-    //     <Stack.Screen name="login" component={Login}></Stack.Screen>
-    //     <Stack.Screen name="Main" component={Main}></Stack.Screen>
-    //   </Stack.Navigator>
-    // </NavigationContainer>
-    <Main></Main>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown :false}}>
+        <Stack.Screen name="login" component={Login}></Stack.Screen>
+        <Stack.Screen name="Main" component={Main}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+    // <Main></Main>
   );
 };
 
